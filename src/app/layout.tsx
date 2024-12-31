2384185d6fda2ee57207a961fb965c15ad571a09
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2024-12-31 12:29:17 +0100

    chore: tagging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from '@next/third-parties/google'
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TGSR8YCFJJ"></Script>
+      <Script id="google-analytics">
+        {
+          `
+          window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-TGSR8YCFJJ');
+        `
+        }
+      </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
